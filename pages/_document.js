e5fa83512e0a54a469261b627ede07616f46a501
goforbg <techydevil@gmail.com>
2021-11-10 11:04:00 +0530

    Adds Adsense tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <GoogleAnalytics.Head />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1051562237299725"
+            crossOrigin="anonymous"
+          ></script>
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
             rel="icon"
