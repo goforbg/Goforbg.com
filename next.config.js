3edd4cad6f41a2e0a185d0a280f6839c75c6a5dd
goforbg <techydevil@gmail.com>
2021-11-10 20:13:06 +0530

    Removed loader issues of gif and images

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+}
