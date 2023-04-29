c034915f4309b2b21da2d4dfacc12e57bcfcde4b
Bharadwaj Giridhar <techydevil@gmail.com>
2023-04-29 13:54:45 +0530

    Adds calendar link

---
+++ pages/calendar.js
@@ -0,0 +1,16 @@
+import {useEffect} from "react";
+
+export default function meet () {
+
+    useEffect(() => {
+        window.location.assign('https://cal.com/goforbg/15min?duration=15')
+    })
+
+    return (<>
+
+        <section className={`py-16 lg:py-20 flex justify-center items-center text-2xl text-white font-bold`}>
+            Redirecting you to pick a time on our calendar...
+        </section>
+
+    </>)
+}
