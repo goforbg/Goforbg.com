2443cbebdc3e330447c9034102919f585bde305b
Bharadwaj Giridhar <techydevil@gmail.com>
2025-12-30 15:54:13 +0530

    Fix: Remove files from out/ before plugin copies them to avoid conflicts

--- scripts/create-dummy-serverless.js
+++ scripts/create-dummy-serverless.js
@@ -4,19 +4,20 @@ const path = require('path')
 // Create dummy .next/serverless/pages structure for Netlify plugin
 // This is a workaround since the plugin expects serverless output
 // but we're using static export (output: 'export')
+// The plugin will copy these files to out/, but since they already exist
+// from static export, we need to remove them from out/ first, then let
+// the plugin copy them back
 
 const nextDir = path.join(process.cwd(), '.next')
 const serverlessDir = path.join(nextDir, 'serverless', 'pages')
+const outDir = path.join(process.cwd(), 'out')
 
 // Create directory structure
 if (!fs.existsSync(serverlessDir)) {
   fs.mkdirSync(serverlessDir, { recursive: true })
 }
 
-// Get all HTML files from out directory
-const outDir = path.join(process.cwd(), 'out')
-
-function copyHtmlFiles(srcDir, destDir, basePath = '') {
+function copyHtmlFiles(srcDir, destDir) {
   if (!fs.existsSync(srcDir)) {
     return
   }
@@ -30,14 +31,19 @@ function copyHtmlFiles(srcDir, destDir, basePath = '') {
 
     if (stat.isDirectory()) {
       // Recursively copy subdirectories
-      const newBasePath = path.join(basePath, file)
       const newDestDir = path.join(destDir, file)
       if (!fs.existsSync(newDestDir)) {
         fs.mkdirSync(newDestDir, { recursive: true })
       }
-      copyHtmlFiles(srcPath, newDestDir, newBasePath)
+      copyHtmlFiles(srcPath, newDestDir)
     } else if (file.endsWith('.html')) {
       // Copy HTML files to .next/serverless/pages structure
+      // Remove from out/ first so plugin can copy it back
+      const outPath = srcPath.replace(serverlessDir, outDir).replace('.next/serverless/pages', 'out')
+      if (fs.existsSync(outPath)) {
+        fs.unlinkSync(outPath)
+        console.log(`✅ Removed ${outPath} to allow plugin to copy from .next/serverless/pages`)
+      }
       fs.copyFileSync(srcPath, destPath)
       console.log(`✅ Copied ${srcPath} to ${destPath}`)
     }
@@ -48,6 +54,9 @@ function copyHtmlFiles(srcDir, destDir, basePath = '') {
 const blogHtml = path.join(outDir, 'blog.html')
 if (fs.existsSync(blogHtml)) {
   const destBlogHtml = path.join(serverlessDir, 'blog.html')
+  // Remove from out/ first
+  fs.unlinkSync(blogHtml)
+  console.log(`✅ Removed blog.html from out/ to allow plugin to copy`)
   fs.copyFileSync(blogHtml, destBlogHtml)
   console.log(`✅ Copied blog.html to .next/serverless/pages/`)
 }
@@ -59,7 +68,23 @@ if (fs.existsSync(blogDir)) {
   if (!fs.existsSync(destBlogDir)) {
     fs.mkdirSync(destBlogDir, { recursive: true })
   }
-  copyHtmlFiles(blogDir, destBlogDir, 'blog')
+  // First copy files to serverless, then remove from out
+  const files = fs.readdirSync(blogDir)
+  files.forEach((file) => {
+    const srcPath = path.join(blogDir, file)
+    const destPath = path.join(destBlogDir, file)
+    if (file.endsWith('.html')) {
+      fs.copyFileSync(srcPath, destPath)
+      console.log(`✅ Copied ${file} to .next/serverless/pages/blog/`)
+      // Remove from out/ so plugin can copy it back
+      fs.unlinkSync(srcPath)
+      console.log(`✅ Removed ${file} from out/blog/ to allow plugin to copy`)
+    }
+  })
+  // Remove empty blog directory from out
+  if (fs.readdirSync(blogDir).length === 0) {
+    fs.rmdirSync(blogDir)
+  }
 }
 
 console.log('✅ Created dummy .next/serverless/pages structure for Netlify plugin')
